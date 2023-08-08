9701ed503e8a9d3627fd44e359b4046897ac9843
shokod <delvinshoko@gmail.com>
2023-08-08 09:02:11 +0200

    homepage

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,8 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 const Nav = () => {
     const isUserLoggedIn = true;
 
-    const [providers, setProviders ]= useState (null);
+    const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
         const setProviders = async () => {
@@ -18,54 +19,114 @@ const Nav = () => {
         }
         setProviders();
     }, [])
-    
-  return (
-    <nav className="flex-between w-full mb-16 pt-3">
-    <Link href='/' className="flex gap-2 flex-center">
-        <Image src="/assets/images/loggo.png" width={60}
-        height={60}
-        alt="EventsHub Logo"
-        className="rounded-full" 
-        />
-        <p className="logo_text"></p>
-    </Link>
-    {/* desktop navigation */}
-    <div className="sm:flex hidden">
-        { isUserLoggedIn ? (
-            <div className="flex gap-3 md:gap-5">
-                <Link href='/create-event'
-                className="blue_btn">
-                    Create Post
-                </Link>
-                <button type="button" onClick={signOut} className="outline_btn">
-                    Sign Out
-                </button>
-                <Link href='/profile'>
-                    <Image src="/assets/images/loggo.png" width={37}
-                    height={37}
-                    alt="profile"
-                    className="rounded-full"
-                    />
-                </Link>
-            </div>
-        ) : (
-            <>
-            {providers && 
-            Object.values(providers).map((provider)=>(
-                <button
-                type="button"
-                key={provider.name}
-                onClick={() => signIn(provider.id)} className="blue_btn"
-                >
-                    Sign In
 
+    return (
+        <nav className="flex-between w-full mb-16 pt-3">
+            <Link href='/' className="flex gap-2 flex-center">
+                <Image src="/assets/images/lg.png" width={60}
+                    height={60}
+                    alt="EventsHub Logo"
+                    className="rounded-full"
+                />
+                <p className="logo_text"></p>
+            </Link>
+            {/* desktop navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href='/create-event'
+                            className="blue_btn">
+                            Create Post
+                        </Link>
+                        <button type="button" onClick={signOut} className="outline_btn">
+                            Sign Out
+                        </button>
+                        <Link href='/profile'>
+                            <Image src="/assets/images/lg.png" width={37}
+                                height={37}
+                                alt="profile"
+                                className="rounded-full"
+                            />
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)} className="blue_btn"
+                                >
+                                    Sign In
+
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+            {/* mobile navigation */}
+            <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/lg.png'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-event'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Event
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full blue_btn'
+                >
+                  Sign Out
                 </button>
-            ))}
-            </>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='blue_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
         )}
-    </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
+
 
 export default Nav
